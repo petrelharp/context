bc8b87c06178a090d483c0e6f71addb24672fd24
peter <peter@phoebe.usc.edu>
2013-03-10 21:18:24 -0700

    .

--- get-all-positions.sh
+++ get-all-positions.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+SCRIPTDIR=/home/peter/projects/codons
+
 codons="GCT GCC GCA GCG CGT CGC CGA CGG AGA AGG AAT AAC GAT GAC TGT TGC CAA CAG GAA GAG GGT GGC GGA GGG CAT CAC ATT ATC ATA TTA TTG CTT CTC CTA CTG AAA AAG ATG TTT TTC CCT CCC CCA CCG TCT TCC TCA TCG AGT AGC ACT ACC ACA ACG TGG TAT TAC GTT GTC GTA GTG"
 
-for chrom in 2 3 4 X
+for chrom in 2L 2R 3L 3R 4 X
 do
-    python get-positions.py -i ${chrom}.raw.gz -l $chrom.raw.patterns.log -o ${chrom}.raw --patterns $codons 
+    python $SCRIPTDIR/get-positions.py -i ${chrom}.raw.gz -l $chrom.raw.patterns.log -o ${chrom}.raw --patterns $codons 
 done
