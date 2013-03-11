a725428cb273bf395bb8fcd7e961d16a2ddaee68
peter <petrel.harp@gmail.com>
2013-03-10 21:43:38 -0700

    is-coding.py

---
+++ is-coding.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python2.7
+
+import argparse
+import sys, gzip, signal
+from plrutils import *
+import collections
+
+parser = argparse.ArgumentParser(description='Write out a collection of files giving the locations of certain sequences.')
+parser.add_argument('--infile', '-i', nargs='?', default="-")
+parser.add_argument('--codingfile', '-c', nargs='?', help="Tab-separated file with starting & ending positions of coding sequence.")
+parser.add_argument('--logfile', '-l', nargs='?', default="-")
+parser.add_argument('--outfix', '-o', default="locations", action='store', help='output will be files prefixed by this')
+
+args = parser.parse_args()
+# example:
+    # args = parser.parse_args("-c CDS-dmel-3R-r5.50.CDS.starts.ends.gz -i 4.rawTAC.gz -o 4.rawTAC -l 4.rawTAC.CDS.log".split())
+
+infile = fileopt(args.infile,"r")
+codingfile = fileopt(args.codingfile,"r")
+orffile = fileopt(args.outfix + ".orfcoding.gz","w")
+nonorffile = fileopt(args.outfix + ".nonorfcoding.gz","w")
+noncodingfile = fileopt(args.outfix + ".noncoding.gz","w")
+
+pos = int(infile.readline())
+cds = map( int, codingfile.readline().split() )
+
+while cds:
+    while pos < cds[0]:
+        noncodingfile.write(str(pos)+"\n")
+        pos = int(infile.readline())
+    while pos < cds[1]:
+        if pos-cds[0]==0:
+            orffile.write(str(pos)+"\n")
+        else:
+            nonorffile.write(str(pos)+"\n")
+        pos = int(infile.readline())
+    cds = map( int, codingfile.readline().split() )
+
+# finish off
+while pos:
+    noncodingfile.write(str(pos)+"\n")
+    pos = int(infile.readline())
+
+raise SystemExit
