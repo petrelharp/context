00b3603b96752f88afee38d194582ffe70f702a0
peter <petrel.harp@gmail.com>
2013-04-17 21:51:36 -0700

    .

--- codon-inference.R
+++ codon-inference.R
@@ -1,5 +1,6 @@
 #!/usr/bin/R
 source("codons.R")
+source("codon-inference-fns.R")
 require(expm)
 
 # size of window on either side of the focal site
@@ -8,38 +9,18 @@ rwin <- 1
 win <- 3
 winlen <- lwin+win+rwin
 
-patterns <- do.call( expand.grid, rep( list(bases), winlen ) )
+# all possible patterns
+patterns <- getpatterns(winlen)
 npatt <- nrow(patterns)
-baseind <- nbases^(0:(winlen-1))
-rownames(patterns) <- apply(patterns,1,paste,collapse="")
-
-source("codon-inference-fns.R")
-
 
 # list of patterns that have mutation rates
 mutpats <- c(
-        combn( bases, 2, simplify=FALSE ),  # single-base rates
-        lapply( combn( bases, 2, simplify=FALSE ), rev),  # single-base rates
+        combn( bases, 2, simplify=FALSE ),  lapply( combn( bases, 2, simplify=FALSE ), rev),  # single-base rates
         list( c("CG","TG"), c("CG","CA") ), # CpG rates
-        # do.call( c, with( subset(codons, aa%in%synons), tapply( levels(codon)[as.numeric(codon)], droplevels(aa), function(x)combn(x, 2, simplify=FALSE) ) ) ),  # synonymous codon rates 
     NULL )
 
-# list of matrices with indices of changes corresponding to mutation patterns above
-mutmats <- lapply( mutpats, function (x) {
-        do.call( rbind, lapply( 1:(winlen-nchar(x[1])+1), function (k) {
-                i <- which( substr( rownames(patterns), k, k+nchar(x[1])-1 ) == x[1] )
-                replstr <- rownames(patterns)[i]
-                substr( replstr, k, k+nchar(x[1])-1 ) <- x[2]
-                j <- match( replstr, rownames(patterns) )
-                data.frame( i=i, j=j )
-            } ) )
-    } )
-
 # mutation rates
-mutrates <- runif( length(mutmats) )*1e-8
-# function to transfer these to list of values in mutation matrix
-nmutswitches <- sapply(mutmats,NROW)
-whichmut <- function (mutrates) { rep(mutrates,times=nmutswitches) }
+mutrates <- runif( length(mutpats) )*1e-8
 
 # list of patterns with selection coefficients
 # can be regexps but only using "." and "[...]" (needs regexplen() to work)
@@ -48,34 +29,23 @@ selpats <- c(
         "[AT]",
         paste( paste(rep(".",lwin),collapse=''), paste( codons$codon[codons$aa %in% synons], paste(rep(".",rwin),collapse=''), sep='' ), sep='' ),
     NULL )
-# number of times each selpat matches each pattern
-selmatches <- do.call( rbind, lapply(selpats, function (x) {
-        rowSums( sapply( 0:(winlen-regexplen(x)), function (k) {
-                    xx <- paste( c(rep(".",k), x, rep(".", winlen-regexplen(x)-k)), collapse='' )
-                    grepl( xx, rownames(patterns) ) 
-            } ) )
-    } ) )
+# selection coefficients
+selcoef <- runif( length(selpats) )*1e-4
 
 # Other params
 Ne <- 1e4
 tlen <- (6e6/20)
 
-# selection coefficients
-selcoef <- runif( length(selpats) )*1e-4
-# function to transfer these to list of values (signed) in transition matrix
-# these are ( transitions ) x ( selpats ) matrix
-fromsel <- selmatches[,  do.call( rbind, mutmats )$i ]
-tosel <- selmatches[,  do.call( rbind, mutmats )$j ]
-seltrans <- tosel - fromsel
-seldiff <- function (selcoef) { as.vector( crossprod(selcoef,seltrans)) }
-fixfn <- function (x,Ne) { ifelse( x==0, 1, expm1(2*x)/expm1(2*Ne*x) ) }
+# full instantaneous mutation, and transition matrix
+# mutmatrix <- with( do.call( rbind, mutmats ), new( "dgTMatrix", i=i-1L, j=j-1L, x=whichmut(mutrates), Dim=c(npatt,npatt) ) )
+genmatrix <- makegenmatrix( mutpats, selpats, patterns )
+genmatrix@x <- update(genmatrix,mutrates,selcoef,Ne)
 
-# full instantaneous transition matrix
-mutmatrix <- with( do.call( rbind, mutmats ), new( "dgTMatrix", i=i-1L, j=j-1L, x=whichmut(mutrates), Dim=c(npatt,npatt) ) )
-transmatrix <- with( do.call( rbind, mutmats ), new( "dgTMatrix", i=i-1L, j=j-1L, x=whichmut(mutrates)*fixfn(seldiff(selcoef),Ne), Dim=c(npatt,npatt) ) )
+# transition matrix
+transmatrix <- expm( tlen*(genmatrix-Diagonal(nrow(genmatrix),rowSums(genmatrix))), method="Higham08" )
 
-# system.time( replicate( 1, expm( transmatrix, method="Higham08" ) ) )
-# with winlen=6 is 114 sec
-# with winlen=5 is 2.7 sec
-# with winlen=4 is .075 sec
+# A <- tlen*genmatrix; system.time( replicate( 1, expm( A, method="Higham08" ) ) )
+# with winlen=6 is 151 sec
+# with winlen=5 is 3.6 sec
+# with winlen=4 is .10 sec
 
