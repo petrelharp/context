a1d7ab0b50cc4ebe820e0a73e52c8676c141082f
peter <petrel.harp@gmail.com>
2013-05-13 14:44:53 -0700

    moved to biostrings for simulation

--- sim-context-fns.R
+++ sim-context-fns.R
@@ -1,4 +1,6 @@
 # Simulate a sequence, with context-dependent mutation rates
+require(Biostrings)
+require(IRanges)
 
 rinitseq <- function (seqlen,bases,basefreqs=rep(1/length(bases),length(bases))) {
     # return a random sequence.  bases can in fact be longer patterns.
@@ -7,9 +9,9 @@ rinitseq <- function (seqlen,bases,basefreqs=rep(1/length(bases),length(bases)))
     patlen <- mean( nchar(bases) )
     rpats <- c()
     while( sum(nchar(rpats))<seqlen ) {
-        rpats <- c( rpats, sample( bases, floor(1.05*seqlen/patlen), replace=TRUE,prob=basefreqs ) )
+        rpats <- c( rpats, sample( bases, floor(1.05*seqlen/patlen), replace=TRUE, prob=basefreqs ) )
     }
-    return( substr( paste( rpats, collapse="" ), 1, seqlen ) )
+    return( DNAString(substr( paste( rpats, collapse="" ), 1, seqlen )) )
 }
 
 simseq <- function (seqlen, tlen, patlen, mutpats, mutrates, selpats, selcoef, Ne, bases=c("A","C","G","T"), count.trans=TRUE, initseq, basefreqs=rep(1/length(bases),length(bases)), ... ) {
@@ -26,6 +28,7 @@ simseq <- function (seqlen, tlen, patlen, mutpats, mutrates, selpats, selcoef, N
     mutrates <- mutrates / (patlen-unlist(mutpatlens)+1)  # avoid multiple counting
     genmatrix@x <- update(genmatrix,mutrates,selcoef,Ne)
     patterns <- rownames(genmatrix)
+    patstrings <- DNAStringSet( patterns )
     # max mutation rate
     stopifnot( inherits(genmatrix,"dgTMatrix") )  # we access @i, @j, and @x directly...
     stopifnot( all(genmatrix>=0) )  # assume this comes WITHOUT the diagonal
@@ -43,22 +46,22 @@ simseq <- function (seqlen, tlen, patlen, mutpats, mutrates, selpats, selcoef, N
     finalseq <- initseq
     for (k in seq_along(loc.events)) {
         subseq <- if (wrap.events[k]) { # from string (cyclical)
-                paste( substr( finalseq, loc.events[k], seqlen), substr(finalseq, 1, loc.events[k]+patlen-seqlen-1), sep='' )
+                xscat( subseq( finalseq, loc.events[k], seqlen), subseq(finalseq, 1, loc.events[k]+patlen-seqlen-1) )
             } else {  
-                substr(finalseq, loc.events[k], loc.events[k]+patlen-1 ) 
+                subseq(finalseq, loc.events[k], loc.events[k]+patlen-1 ) 
             }
-        msubseq <- match( subseq, patterns )  # which row for this string?
+        msubseq <- match( subseq, patstrings )  # which row for this string?
         isubseq <- which( (genmatrix@i + 1) == msubseq ) # transitions are these entries of genmatrix
         jsubseq <- c( msubseq, (genmatrix@j[isubseq]+1) ) # indices of possible replacement patterns (self is first)
         psubseq <- c( diags[msubseq], genmatrix@x[isubseq] )  # probabilities of choosing these
         replind <- sample( jsubseq, 1, prob=psubseq ) # choose this replacement string (index)
-        replstr <- patterns[replind] # what is the replacement string
-        if (count.trans) {  ntrans$i[k] <- subseq; ntrans$j[k] <- replstr } # record this (is a factor so not storing actual strings)
+        replstr <- patstrings[[replind]] # what is the replacement string
+        if (count.trans) {  ntrans$i[k] <- patterns[msubseq]; ntrans$j[k] <- patterns[replind] } # record this (is a factor so not storing actual strings)
         if (wrap.events[k]) { # put this back in (cyclical)
-            substr( finalseq, loc.events[k], seqlen) <- substr(replstr,1,seqlen-loc.events[k]+1)
-            substr(finalseq, 1, loc.events[k]+patlen-seqlen) <- substr(replstr,seqlen-loc.events[k]+2,patlen)
+            subseq( finalseq, loc.events[k], seqlen) <- subseq(replstr,1,seqlen-loc.events[k]+1)
+            subseq(finalseq, 1, loc.events[k]+patlen-seqlen) <- subseq(replstr,seqlen-loc.events[k]+2,patlen)
         } else {
-            substr( finalseq, loc.events[k], loc.events[k]+patlen-1 ) <- replstr
+            subseq( finalseq, loc.events[k], loc.events[k]+patlen-1 ) <- replstr
         }
     }
     output <-  list( initseq=initseq, finalseq=finalseq, maxrate=maxrate, ntrans=ntrans ) 
@@ -70,11 +73,8 @@ countpats <- function (patterns, simseq ) {
     # count number of times each pattern appears in character string simseq, cyclic-ized.
     patlen <- nchar(patterns[1])
     seqlen <- nchar(simseq)
-    simseq <- paste(simseq,substr(simseq,1,patlen-1),sep='') # cyclic-ize
-    matches <- lapply( patterns, function (p) gregexpr(paste("(?=",p,")",sep=''),simseq,perl=TRUE) )
-    counts <-  sapply(matches,function(x) length(x[[1]]) )
-    names(counts) <- patterns
-    return(counts)
+    simseq <- xscat( simseq, subseq(simseq,1,patlen-1) ) # cyclic-ize
+    return( sapply( patterns, countPattern, simseq ) )
 }
 
 counttrans <- function (ipatterns, fpatterns, initseq=simseqs[["initseq"]], finalseq=simseqs[["finalseq"]], simseqs, lwin=0) {
@@ -84,16 +84,15 @@ counttrans <- function (ipatterns, fpatterns, initseq=simseqs[["initseq"]], fina
     seqlen <- nchar(initseq)
     stopifnot( seqlen == nchar(finalseq) )
     # cyclic-ize
-    initseq <- paste(initseq,substr(initseq,1,patlen-1),sep='')
-    finalseq <- paste(finalseq,substr(finalseq,1,patlen-1),sep='')
-    # Ok, count occurrences.  Note needs perl "lookahead" to count overlapping patterns.
-    #   (see http://stackoverflow.com/questions/7878992/finding-the-indexes-of-multiple-overlapping-matching-substrings)
-    initmatches <- lapply( ipatterns, function (p) gregexpr(paste("(?=",p,")",sep=''),initseq,perl=TRUE) )
-    finalmatches <- lapply( fpatterns, function (p) gregexpr(paste("(?=",p,")",sep=''),finalseq,perl=TRUE) )
+    initseq <- xscat( initseq, subseq(initseq,1,patlen-1) )
+    finalseq <- xscat( finalseq, subseq(finalseq,1,patlen-1) )
+    # Ok, count occurrences.  
+    initmatches <- lapply( ipatterns, matchPattern, initseq )
+    finalmatches <- lapply( fpatterns, matchPattern, finalseq )
     counts <- Matrix( 0, nrow=length(initmatches), ncol=length(finalmatches), dimnames=list(ipatterns,fpatterns) )
     for (x in seq_along(initmatches))  {
         for (y in seq_along(finalmatches)) {
-            counts[x,y] <- length(intersect(initmatches[[x]][[1]],(-lwin)+finalmatches[[y]][[1]]))
+            counts[x,y] <- length(intersect(start(initmatches[[x]]),(-lwin)+start(finalmatches[[y]])))
         } 
     }
     return(counts)
@@ -101,25 +100,23 @@ counttrans <- function (ipatterns, fpatterns, initseq=simseqs[["initseq"]], fina
 
 show.simseq <- function (x,printit=FALSE,maxchar=1e8) { 
     # display the output of simulation by lowercasing or replacing with "." bases that didn't change
-    x$initseq <- substr(x$initseq,1,maxchar)
-    x$finalseq <- substr(x$finalseq,1,maxchar)
+    x$initseq <- subseq(x$initseq,1,maxchar)
+    fseq <- BString( subseq(x$finalseq,1,maxchar) )
     x$ntrans <- subset(x$ntrans,loc<=maxchar)
     patlen <- nchar( levels( x$ntrans$i )[1] )
-    chars <- strsplit(x$finalseq,'')[[1]]
     events <- unique(x$ntrans$loc)
-    eventlocs <- (seq_along(chars) %in% outer(events,0:(patlen-1),"+"))
+    eventlocs <- (seq_len(nchar(fseq)) %in% outer(events,0:(patlen-1),"+"))
     if (length(events)>0) {
         whichchanged <- sapply( events, function (k) { kev <- (x$ntrans$loc==k); any( x$ntrans$i[kev] != x$ntrans$j[kev] ) } )
-        changedlocs <- ( seq_along(chars) %in% outer( events[whichchanged], 0:(patlen-1), "+" ) )
-        chars[eventlocs & !changedlocs] <- tolower(chars[eventlocs & !changedlocs])
+        changedlocs <- ( seq_len(nchar(fseq)) %in% outer( events[whichchanged], 0:(patlen-1), "+" ) )
+        fseq[eventlocs & !changedlocs] <- tolower(fseq[eventlocs & !changedlocs])
     }
-    chars[!eventlocs] <- "."
-    out <- paste( chars, collapse='' )
+    fseq[!eventlocs] <- "."
     if (printit) {
-        cat(paste(substr(x$initseq,1,maxchar),"\n",sep=''))
-        cat(paste(substr(out,1,maxchar),"\n",sep=''))
+        cat(paste(as.character(x$initseq),"\n",sep=''))
+        cat(paste(as.character(fseq),"\n",sep=''))
     }
-    return(invisible(out))
+    return(invisible(fseq))
 } 
 
 print.simseq <- function (x,...) {
