fb95ddeb3e797e817b9aa100922db703cddf225a
peter <petrel.harp@gmail.com>
2013-05-24 11:53:07 -0700

    counting patterns

--- ising/ising-bayesian.R
+++ ising/ising-bayesian.R
@@ -5,12 +5,33 @@ set.seed(42)
 # mean of exponential prior on mutation rate
 pmean <- 1
 
+####
+# available simulated sequences
+simdir <- "ising-sims"
+simfiles <- list.files(simdir,"*.RData",full.names=TRUE)
+siminfo <- do.call(rbind, lapply(simfiles, function (x) {
+        load(x)
+        y <- do.call( data.frame, c(list(date=now,seqlen=seqlen, tlen=tlen, file=x), as.list(mutrates), as.list(selcoef), stringsAsFactors=FALSE) )
+        colnames(y) <- c( colnames(y)[1:4], paste("mutrate",seq_along(mutrates),sep=''), paste("selcoef",seq_along(selcoef),sep='') )
+        y
+    } ) )
+siminfo$meandist <- siminfo$tlen * colSums(siminfo[,grep("mutrate",colnames(siminfo)),drop=FALSE])
+siminfo <- siminfo[ order(siminfo$meandist), ]
+
+win <- 2
+
+# pick one
+thissim <- siminfo[1,"file"]
+
+load(thissim)
+
+######
 # Inference.
 lwin <- rwin <- thiswin
 winlen <- lwin+win+rwin
 
-genmatrix <- makegenmatrix( mutpats, selpats=c(), patlen=winlen, boundary="wrap")
-genmatrix@x <- update(genmatrix,mutrates,selcoef=numeric(0),Ne=1)
+genmatrix <- makegenmatrix( mutpats, selpats=selpats, patlen=winlen, boundary="wrap")
+genmatrix@x <- update(genmatrix,mutrates,selcoef=selcoef,Ne=1)
 projmatrix <- collapsepatmatrix( ipatterns=rownames(genmatrix), lwin=lwin, rwin=rwin )
 subtransmatrix <- computetransmatrix( genmatrix, projmatrix, names=TRUE )
 
@@ -19,25 +40,12 @@ counts <- list(
         )
 # allow nonmatches with small prob?
 eps <- 0
-
-lud <- function (mutrates) {
-    if (mutrates <= 0) { 
-        return( -Inf )
-    } else {
-        # params are: Ne, branchlens[-1], mutrates, selcoef, basefreqs
-        # this is collapsed transition matrix
-        genmatrix@x <- update(genmatrix,mutrates,selcoef=numeric(0),Ne=1)
-        subtransmatrix <- computetransmatrix( genmatrix, projmatrix )
-        # return POSITIVE log-likelihood 
-        return( sum( counts[[1]] * log(eps+subtransmatrix) ) + pmean * mutrates )
-    }
-}
-
 nmuts <- length(mutrates)
 nsel <- length(selcoef)
+pmeans <- rep(pmean,nmuts+nsel)
 # move from base frequencies (what we estimate) to pattern frequencies
 lud <- function (params) {
-    if (any(params)<0) {
+    if (any(params<0)) {
         return( -Inf )
     } else {
         # params are: mutrates, selcoef
@@ -52,5 +60,5 @@ lud <- function (params) {
 }
 
 
-system.time( mrun <- metrop( lud, initial=truth, nbatch=20, blen=10 ) )
+system.time( mrun <- metrop( lud, initial=truth, nbatch=1000, blen=10, scale=1e-2 ) )
 
