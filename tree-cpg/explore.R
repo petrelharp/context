9e3d39f80ca17b0d4562d1be9d6123eaaf58f396
peter <petrel.harp@gmail.com>
2013-06-10 17:20:10 -0700

    .

---
+++ tree-cpg/explore.R
@@ -0,0 +1,42 @@
+# mess around with stuff
+
+scriptdir <- "../"
+source(paste(scriptdir,"codon-inference-fns.R",sep=''))
+source(paste(scriptdir,"sim-context-fns.R",sep=''))
+require(mcmc)
+
+# infile <- "cpg-tree-sims/selsims-2013-06-03-13-17-0356374.RData"
+for (infile in list.files("cpg-tree-sims","*RData",full.names=TRUE)) {
+
+    basedir <- gsub(".RData","",infile,fixed=TRUE)
+    load(infile)
+
+    lwin <- rwin <- 2; win <- 1
+    basename <- paste(basedir,"/win-",lwin,"-",win,"-",rwin,sep='')
+    datafile <- paste( basename ,"-results.RData",sep='')
+    plotfile <- paste( basename ,"-plot-all-mcmcs",sep='')
+    mcmcdatafiles <- list.files(path=basedir,pattern="-mcmc.*RData",full.names=TRUE)
+    mcmcnum <- 1+max(c(0,as.numeric(gsub(".*-mcmc-","",gsub(".RData","",mcmcdatafiles)))),na.rm=TRUE)
+
+    load(datafile)
+
+    all.mrun <- do.call( rbind, lapply( seq_along(mcmcdatafiles), function (k) {
+                load(mcmcdatafiles[[k]])
+                tmp <- data.frame( mrun$batch )
+                names(tmp) <- c("branchlen", paste("mut:", unlist( sapply( sapply( mutpats, lapply, paste, collapse="->" ), paste, collapse=" | " ) ) ), names(initfreqs)[-length(initfreqs)] )
+                tmp$mcmcnum <- k
+                tmp
+            } ) )
+
+    subseq <- seq(1,nrow(all.mrun),by=max(1,floor(nrow(all.mrun)/1000)))
+
+    x <- rbind( all.mrun[subseq,(-1)*match("mcmcnum",colnames(all.mrun))], truth ) 
+
+    # # color by run
+    # cols <- c( adjustcolor(rainbow(7),.2)[all.mrun$mcmcnum[subseq]], "black" )
+    # color by time
+    cols <- c( adjustcolor(rainbow(64),.2)[1+floor(65*(1:(nrow(x)-1))/nrow(x))], adjustcolor("black",.5) )
+    pdf(file=paste(plotfile,".pdf",sep=''),width=12,height=12,pointsize=10)
+    pairs( x, col=cols, pch=20, cex=c(rep(.5,nrow(x)-1),2) )
+    dev.off()
+}
