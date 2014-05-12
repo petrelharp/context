84534a5e830d7965ec731d16da59f3985338a4bb
peter <petrel.harp@gmail.com>
2014-05-12 12:16:31 -0700

    better tuple counting

---
+++ bcells/count-many-tuples.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+basename=$1
+fasta="${basename}.fasta"
+
+w=5; lr=2; python ../tuple-counting/count-paired-tuples.py -w ${w} -l ${lr} -r ${lr} -i $fasta > $basename/tuples.${w}.${lr}.counts
+w=5; lr=1; python ../tuple-counting/count-paired-tuples.py -w ${w} -l ${lr} -r ${lr} -i $fasta > $basename/tuples.${w}.${lr}.counts
+w=6; lr=1; python ../tuple-counting/count-paired-tuples.py -w ${w} -l ${lr} -r ${lr} -i $fasta > $basename/tuples.${w}.${lr}.counts
+w=6; lr=2; python ../tuple-counting/count-paired-tuples.py -w ${w} -l ${lr} -r ${lr} -i $fasta > $basename/tuples.${w}.${lr}.counts
+w=7; lr=3; python ../tuple-counting/count-paired-tuples.py -w ${w} -l ${lr} -r ${lr} -i $fasta > $basename/tuples.${w}.${lr}.counts
+w=7; lr=2; python ../tuple-counting/count-paired-tuples.py -w ${w} -l ${lr} -r ${lr} -i $fasta > $basename/tuples.${w}.${lr}.counts
+w=8; lr=3; python ../tuple-counting/count-paired-tuples.py -w ${w} -l ${lr} -r ${lr} -i $fasta > $basename/tuples.${w}.${lr}.counts
+w=8; lr=2; python ../tuple-counting/count-paired-tuples.py -w ${w} -l ${lr} -r ${lr} -i $fasta > $basename/tuples.${w}.${lr}.counts
+w=9; lr=3; python ../tuple-counting/count-paired-tuples.py -w ${w} -l ${lr} -r ${lr} -i $fasta > $basename/tuples.${w}.${lr}.counts
