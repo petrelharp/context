812befaaf10b8d344797c2a5c565eae86d089ade
peter <petrel.harp@gmail.com>
2014-05-12 12:20:02 -0700

    works now

--- bcells/count-many-tuples.sh
+++ bcells/count-many-tuples.sh
@@ -3,6 +3,11 @@
 basename=$1
 fasta="${basename}.fasta"
 
+if [ ! -d "$basename" ]
+then
+    mkdir $basename
+fi
+
 w=5; lr=2; python ../tuple-counting/count-paired-tuples.py -w ${w} -l ${lr} -r ${lr} -i $fasta > $basename/tuples.${w}.${lr}.counts
 w=5; lr=1; python ../tuple-counting/count-paired-tuples.py -w ${w} -l ${lr} -r ${lr} -i $fasta > $basename/tuples.${w}.${lr}.counts
 w=6; lr=1; python ../tuple-counting/count-paired-tuples.py -w ${w} -l ${lr} -r ${lr} -i $fasta > $basename/tuples.${w}.${lr}.counts
