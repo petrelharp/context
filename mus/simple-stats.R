44df311b4c3755c42f2045fda50db02c5cfaba55
peter <petrel.harp@gmail.com>
2014-05-31 07:49:26 -0700

    plot stats

--- mus/simple-stats.R
+++ mus/simple-stats.R
@@ -1,5 +1,6 @@
 #!/usr/bin/Rscript --vanilla
 require(optparse)
+require(colorspace)
 
 usage <- "\
 Plot simple statistics for count files in some directory.
@@ -11,8 +12,8 @@ option_list <- list(
         make_option( c("-o","--outfile"), type="character", default="", help="Direct output to this file. [default \"simple-stats-win-lwin-rwin.pdf\"]" ),
         make_option( c("-g","--gmfile"), type="character", default="TRUE", help="File with precomputed generator matrix, or TRUE [default] to look for one. (otherwise, will compute)"),
     # context and pattern size
-        make_option( c("-w","--win"), type="integer", default=1, help="Size of matching window. [default \"%default\"]" ),
-        make_option( c("-l","--lrwin"), type="integer", default=2, help="Size of left-hand and right-hand context. [default \"%default\"]" )
+        make_option( c("-w","--win"), type="integer", default=3, help="Size of matching window. [default \"%default\"]" ),
+        make_option( c("-l","--lrwin"), type="integer", default=1, help="Size of left-hand and right-hand context. [default \"%default\"]" )
     )
 opt <- parse_args(OptionParser(option_list=option_list,description=usage))
 if (is.null(opt$basedir)) { stop("No input directory  Run\n  bcells-inference.R -h\n for help.\n") }
@@ -21,7 +22,7 @@ attach(opt)
 lwin <- rwin <- lrwin
 winlen <- win + lwin + rwin
 
-if (opt$outfile=="") { outfile <- with(opt, paste(basedir,"simple-stats",win,"-",lwin,"-",rwin,".pdf",sep='')) }
+if (opt$outfile=="") { outfile <- with(opt, paste(basedir,"/simple-stats",win,"-",lwin,"-",rwin,".pdf",sep='')) }
 
 scriptdir <- "../"
 source(paste(scriptdir,"context-inference-fns.R",sep=''))
@@ -38,8 +39,8 @@ if (file.exists(gmfile)) {
 projmatrix <- collapsepatmatrix( ipatterns=rownames(genmatrix), lwin=lwin, rwin=rwin )
 subtransmatrix <- computetransmatrix( genmatrix, projmatrix, names=TRUE, time="gamma" )
 
-countfiles <- list.files(basedir,paste("*",win,lrwin,"counts",sep="."), full.names=TRUE)
-names(countfiles) <- gsub(".*chr([0-9]*).*","chr\\1",countfiles)
+countfiles <- list.files(basedir,paste("*",winlen,lrwin,"counts",sep="."), full.names=TRUE)
+names(countfiles) <- gsub(".*chr([0-9XY]*).*","chr\\1",countfiles)
 countfiles <- countfiles[ order( as.numeric( gsub("chr","",names(countfiles)) ) ) ]
 counts <- lapply(countfiles, function (x) {
         count.table <- read.table(x,header=TRUE,stringsAsFactors=FALSE)
@@ -57,16 +58,22 @@ adhoc <- sapply( counts, function (counts) {
     } )
 rownames(adhoc) <- mutnames <- sapply(mutpats,function(x){paste(sapply(x,paste,collapse='->'),collapse='/')})
 
+mean.counts <- Reduce("+",counts)/length(counts)
+
 pdf(file=outfile, width=10, height=7.5, pointsize=10)
 layout(matrix(1:min(ncol(counts),6),nrow=2))
 par(mar=c(5,2,1,1)+.1)
-matplot( adhoc, type='l', ylab='mutation rate', xlab='', xaxt='n' )
+cols <- rainbow_hcl(24)
+matplot( adhoc, type='l', ylab='mutation rate', xlab='', xaxt='n', col=cols )
 axis(1, at=1:nrow(adhoc), labels=mutnames, las=3 )
-for (k in 1:ncol(counts)) {
-    lord <- order( subexpected[,k] )
-    plot( counts[lord,k], xaxt='n', xlab='', main=colnames(counts)[k], log='y', ylim=1+range(c(as.matrix(counts),as.matrix(subexpected))), ylab='' )
-    axis(1,at=1:nrow(counts),labels=rownames(counts)[lord],las=3)
-    lines(subexpected[lord,k],col='red')
-    legend("topleft",legend='fitted',lty=1,col='red')
+for (k in 1:ncol(mean.counts)) {
+    lord <- order( mean.counts[,k] )
+    plot( mean.counts[lord,k], xaxt='n', xlab='', main=colnames(mean.counts)[k], log='y', ylim=1+range(unlist(sapply(counts,as.numeric))), ylab='' )
+    axis(1,at=1:nrow(mean.counts),labels=rownames(mean.counts)[lord],las=3)
+    for (j in seq_along(counts)) {
+                points( counts[[j]][lord,k], col=adjustcolor(cols[j],.5), cex=.5, pch=20 ) 
+                lines( lowess(seq_along(lord),counts[[j]][lord,k],f=.05), col=adjustcolor(cols[j],.5), lwd=2 )
+            }
+    legend("topleft",legend=names(counts),lty=1,col=cols)
 }
 dev.off()
