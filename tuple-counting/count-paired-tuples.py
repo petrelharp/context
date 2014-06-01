feed8d42c309cf1ba54b70f057b00b3b0226134a
peter <petrel.harp@gmail.com>
2014-06-01 07:03:27 -0700

    count reverse tuples also

--- tuple-counting/count-paired-tuples.py
+++ tuple-counting/count-paired-tuples.py
@@ -19,6 +19,10 @@ Then count paired tuples of the form:
     llMMMMrrr
 where the  length of the W's is 'winlen', the length of the l's is 'lwin', and the length of the 'r's is 'rwin'.
 
+If --reverse, also count in the other direction, i.e.
+    llMMMMrrr
+    WWWWWWWWW
+
 The longer window (Ws) matches the second (reference) sequence, and the shorter window (M's) matches the first sequence.
 
 If 'strict' then ignores anything but ACGT (case-sensitive).
@@ -28,6 +32,7 @@ parser = argparse.ArgumentParser(description=usage)
 parser.add_argument('--infile', '-i', nargs='?', default="-")
 parser.add_argument('--informat', '-f', nargs='?', default="fasta")
 parser.add_argument('--outfile', '-o', nargs='?', default="-")
+parser.add_argument('--reverse', '-v', action="store_true")
 parser.add_argument('--winlen', '-w', nargs=1)
 parser.add_argument('--lwin', '-l', nargs=1)
 parser.add_argument('--rwin', '-r', nargs=1)
@@ -53,8 +58,11 @@ if (midwin<=0):
 strict = args.strict
 
 outfile = fileopt(args.outfile, "w" )
-
 tuplecount = Counter()
+
+if args.reverse:
+    revfile = fileopt("rev."+args.outfile, "w" )
+    revcount = Counter()
 
 if args.informat == "fasta":
     infile = PairedFastaFile( args.infile )
@@ -73,6 +81,13 @@ while True:
                 next
             else:
                 tuplecount[ ( word1, word2 ) ] += 1
+            if args.reverse:
+                word1 = infile.seq1[x:(x+winlen)]
+                word2 = infile.seq2[(x+lwin):(x+lwin+midwin)]
+                if strict and ( re.search("[^ACGT]",word1) or re.search("[^ACGT]",word2) ):
+                    next
+                else:
+                    revcount[ ( word1, word2 ) ] += 1
     except StopIteration:
         break
 
@@ -82,8 +97,16 @@ for x in tuplecount:
     # omit counts with missing values
     if not any([ 'N' in y for y in x ]):
         outfile.write("\t".join(list(x))+"\t"+str(tuplecount[x])+"\n")
+outfile.close()
+
+if args.reverse:
+    revfile.write("\t".join(["reference","derived","count"])+"\n")
+    for x in revcount:
+        # omit counts with missing values
+        if not any([ 'N' in y for y in x ]):
+            revfile.write("\t".join(list(x))+"\t"+str(revcount[x])+"\n")
+    revfile.close()
 
 infile.close()
-outfile.close()
 
 raise SystemExit
