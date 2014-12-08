9e9403ef0459f78941a0a0ef7e9af5dd73f15d4b
peter <petrel.harp@gmail.com>
2014-12-08 10:06:42 -0800

    .

--- json-ising/many-sims.sh
+++ json-ising/many-sims.sh
@@ -3,6 +3,8 @@
 set -eu
 set -o pipefail
 
+source /home/rcf-40/pralph/cmb/bin/R-setup-usc.sh
+
 # precompute generator matrices:
 Rscript ../make-genmat.R -c genmatrices/complete.json -w 3
 Rscript ../make-genmat.R -c genmatrices/complete.json -w 4
@@ -12,24 +14,25 @@ Rscript ../make-genmat.R -c genmatrices/complete.json -w 6
 for N in $(seq 16)
 do
     (
-        echo "Simulation: $N";
+        DIR=$RANDOM;
+        echo "Simulation $N, in simseqs/$DIR";
         # simulate up some sequence for testing;
-        Rscript ../sim-seq.R -c ising-model.json -t .1 -s 100000 -d simseqs/sim-$N -o test-ising.RData;
+        Rscript ../sim-seq.R -c ising-model.json -t .1 -s 100000 -d simseqs/sim-$DIR -o test-ising.RData;
         # and count the Tmers;
-        Rscript ../count-seq.R -i simseqs/sim-$N/test-ising.RData -w 3 -s 1 -l 1;
-        Rscript ../count-seq.R -i simseqs/sim-$N/test-ising.RData -w 4 -s 2 -l 1;
-        Rscript ../count-seq.R -i simseqs/sim-$N/test-ising.RData -w 5 -s 3 -l 1;
-        Rscript ../count-seq.R -i simseqs/sim-$N/test-ising.RData -w 6 -s 2 -l 2;
+        Rscript ../count-seq.R -i simseqs/sim-$DIR/test-ising.RData -w 3 -s 1 -l 1;
+        Rscript ../count-seq.R -i simseqs/sim-$DIR/test-ising.RData -w 4 -s 2 -l 1;
+        Rscript ../count-seq.R -i simseqs/sim-$DIR/test-ising.RData -w 5 -s 3 -l 1;
+        Rscript ../count-seq.R -i simseqs/sim-$DIR/test-ising.RData -w 6 -s 2 -l 2;
         # fit the model;
-        Rscript ../fit-model.R -i simseqs/sim-$N/test-ising-3-root-1-tip-l1-shift0.counts -m genmatrices/genmatrix-3-complete.RData -o simseqs/sim-$N/test-ising-fit-3-1-1.RData;
-        Rscript ../fit-model.R -i simseqs/sim-$N/test-ising-4-root-2-tip-l1-shift0.counts -m genmatrices/genmatrix-4-complete.RData -o simseqs/sim-$N/test-ising-fit-4-2-1.RData;
-        Rscript ../fit-model.R -i simseqs/sim-$N/test-ising-5-root-3-tip-l1-shift0.counts -m genmatrices/genmatrix-5-complete.RData -o simseqs/sim-$N/test-ising-fit-5-3-1.RData;
-        Rscript ../fit-model.R -i simseqs/sim-$N/test-ising-6-root-2-tip-l2-shift0.counts -m genmatrices/genmatrix-6-complete.RData -o simseqs/sim-$N/test-ising-fit-6-2-2.RData;
+        Rscript ../fit-model.R -i simseqs/sim-$DIR/test-ising-3-root-1-tip-l1-shift0.counts -m genmatrices/genmatrix-3-complete.RData -o simseqs/sim-$DIR/test-ising-fit-3-1-1.RData;
+        Rscript ../fit-model.R -i simseqs/sim-$DIR/test-ising-4-root-2-tip-l1-shift0.counts -m genmatrices/genmatrix-4-complete.RData -o simseqs/sim-$DIR/test-ising-fit-4-2-1.RData;
+        Rscript ../fit-model.R -i simseqs/sim-$DIR/test-ising-5-root-3-tip-l1-shift0.counts -m genmatrices/genmatrix-5-complete.RData -o simseqs/sim-$DIR/test-ising-fit-5-3-1.RData;
+        Rscript ../fit-model.R -i simseqs/sim-$DIR/test-ising-6-root-2-tip-l2-shift0.counts -m genmatrices/genmatrix-6-complete.RData -o simseqs/sim-$DIR/test-ising-fit-6-2-2.RData;
         # look at results;
-        ../templated-Rmd.sh ../simulation.Rmd simseqs/sim-$N/test-ising-fit-3-1-1.RData simseqs/sim-$N/test-ising.RData;
-        ../templated-Rmd.sh ../simulation.Rmd simseqs/sim-$N/test-ising-fit-4-2-1.RData simseqs/sim-$N/test-ising.RData;
-        ../templated-Rmd.sh ../simulation.Rmd simseqs/sim-$N/test-ising-fit-5-3-1.RData simseqs/sim-$N/test-ising.RData;
-        ../templated-Rmd.sh ../simulation.Rmd simseqs/sim-$N/test-ising-fit-6-2-2.RData simseqs/sim-$N/test-ising.RData;
+        ../templated-Rmd.sh ../simulation.Rmd simseqs/sim-$DIR/test-ising-fit-3-1-1.RData simseqs/sim-$DIR/test-ising.RData;
+        ../templated-Rmd.sh ../simulation.Rmd simseqs/sim-$DIR/test-ising-fit-4-2-1.RData simseqs/sim-$DIR/test-ising.RData;
+        ../templated-Rmd.sh ../simulation.Rmd simseqs/sim-$DIR/test-ising-fit-5-3-1.RData simseqs/sim-$DIR/test-ising.RData;
+        ../templated-Rmd.sh ../simulation.Rmd simseqs/sim-$DIR/test-ising-fit-6-2-2.RData simseqs/sim-$DIR/test-ising.RData;
     ) &
 done
 
