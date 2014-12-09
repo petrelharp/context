99c57354e023302a484a7e0f1ec7684486e48dc5
peter <petrel.harp@gmail.com>
2014-12-09 12:41:13 -0800

    scripts

---
+++ json-cpg/collect-many-sims.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SIMDIRS=$(find simseqs -type d -name "sim-*")
+
+for DIR in $SIMDIRS
+do
+    SIMFITS=$(find $DIR -name "test-cpg-fit*.RData")
+    for SIMFIT in $SIMFITS
+    do
+        OUTFILE=$(echo $SIMFIT | sed -e 's/RData$/json/')
+        echo "Reading from ${SIMFIT}, writing to ${OUTFILE}."
+        Rscript ../gather-results.R --fit $SIMFIT --sim $DIR/test-ising.RData --outfile $OUTFILE --json 2>/dev/null &
+        while (( $(jobs 2>&1 | grep -c Running) >= 8 )); do sleep 0.1; done
+    done
+done
+
--- json-ising/many-sims.sh
+++ json-ising/many-sims.sh
@@ -3,6 +3,7 @@
 set -eu
 set -o pipefail
 
+cd /home/rcf-40/pralph/panfs/context/json-ising
 source /home/rcf-40/pralph/cmb/bin/R-setup-usc.sh
 
 # precompute generator matrices:
@@ -14,8 +15,9 @@ Rscript ../make-genmat.R -c genmatrices/complete.json -w 6
 for N in $(seq 16)
 do
     (
-        DIR=$RANDOM;
+        DIR=$(printf "%05g" $RANDOM);
         echo "Simulation $N, in simseqs/$DIR";
+        mkdir -p simseqs/sim-$DIR
         # simulate up some sequence for testing;
         Rscript ../sim-seq.R -c ising-model.json -t .1 -s 100000 -d simseqs/sim-$DIR -o test-ising.RData;
         # and count the Tmers;
@@ -28,11 +30,6 @@ do
         Rscript ../fit-model.R -i simseqs/sim-$DIR/test-ising-4-root-2-tip-l1-shift0.counts -m genmatrices/genmatrix-4-complete.RData -o simseqs/sim-$DIR/test-ising-fit-4-2-1.RData;
         Rscript ../fit-model.R -i simseqs/sim-$DIR/test-ising-5-root-3-tip-l1-shift0.counts -m genmatrices/genmatrix-5-complete.RData -o simseqs/sim-$DIR/test-ising-fit-5-3-1.RData;
         Rscript ../fit-model.R -i simseqs/sim-$DIR/test-ising-6-root-2-tip-l2-shift0.counts -m genmatrices/genmatrix-6-complete.RData -o simseqs/sim-$DIR/test-ising-fit-6-2-2.RData;
-        # look at results;
-        ../templated-Rmd.sh ../simulation.Rmd simseqs/sim-$DIR/test-ising-fit-3-1-1.RData simseqs/sim-$DIR/test-ising.RData;
-        ../templated-Rmd.sh ../simulation.Rmd simseqs/sim-$DIR/test-ising-fit-4-2-1.RData simseqs/sim-$DIR/test-ising.RData;
-        ../templated-Rmd.sh ../simulation.Rmd simseqs/sim-$DIR/test-ising-fit-5-3-1.RData simseqs/sim-$DIR/test-ising.RData;
-        ../templated-Rmd.sh ../simulation.Rmd simseqs/sim-$DIR/test-ising-fit-6-2-2.RData simseqs/sim-$DIR/test-ising.RData;
     ) &
 done
 
