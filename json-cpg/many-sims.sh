e9192ef1cf095ba2161bd43d801fa6e8193834a3
peter <petrel.harp@gmail.com>
2014-12-10 09:00:20 -0800

    better scaling parameters in config

--- json-cpg/many-sims.sh
+++ json-cpg/many-sims.sh
@@ -30,16 +30,16 @@ do
         Rscript ../count-seq.R -i simseqs/sim-$DIR/test-cpg.RData -w 6 -s 2 -l 2;
 
         # fit the model;
-        Rscript ../fit-model.R -i simseqs/sim-$DIR/test-cpg-3-root-1-tip-l1-shift0.counts -m genmatrices/genmatrix-3-cpg.RData -o simseqs/sim-$DIR/test-cpg-fit-3-1-1.RData;
+        Rscript ../fit-model.R -c $MODEL -i simseqs/sim-$DIR/test-cpg-3-root-1-tip-l1-shift0.counts -m genmatrices/genmatrix-3-cpg.RData -o simseqs/sim-$DIR/test-cpg-fit-3-1-1.RData;
         Rscript ../gather-results.R --fit simseqs/sim-$DIR/test-cpg-fit-3-1-1.RData --sim simseqs/sim-${DIR}/test-cpg.RData --outfile simseqs/sim-$DIR/test-cpg-fit-3-1-1.json --json 2>/dev/null 
 
-        Rscript ../fit-model.R -i simseqs/sim-$DIR/test-cpg-4-root-2-tip-l1-shift0.counts -m genmatrices/genmatrix-4-cpg.RData -o simseqs/sim-$DIR/test-cpg-fit-4-2-1.RData;
+        Rscript ../fit-model.R -c $MODEL -i simseqs/sim-$DIR/test-cpg-4-root-2-tip-l1-shift0.counts -m genmatrices/genmatrix-4-cpg.RData -o simseqs/sim-$DIR/test-cpg-fit-4-2-1.RData;
         Rscript ../gather-results.R --fit simseqs/sim-$DIR/test-cpg-fit-4-2-1.RData --sim simseqs/sim-${DIR}/test-cpg.RData --outfile simseqs/sim-$DIR/test-cpg-fit-4-2-1.json --json 2>/dev/null 
 
-        Rscript ../fit-model.R -i simseqs/sim-$DIR/test-cpg-5-root-3-tip-l1-shift0.counts -m genmatrices/genmatrix-5-cpg.RData -o simseqs/sim-$DIR/test-cpg-fit-5-3-1.RData;
+        Rscript ../fit-model.R -c $MODEL -i simseqs/sim-$DIR/test-cpg-5-root-3-tip-l1-shift0.counts -m genmatrices/genmatrix-5-cpg.RData -o simseqs/sim-$DIR/test-cpg-fit-5-3-1.RData;
         Rscript ../gather-results.R --fit simseqs/sim-$DIR/test-cpg-fit-5-3-1.RData --sim simseqs/sim-${DIR}/test-cpg.RData --outfile simseqs/sim-$DIR/test-cpg-fit-5-3-1.json --json 2>/dev/null 
 
-        Rscript ../fit-model.R -i simseqs/sim-$DIR/test-cpg-6-root-2-tip-l2-shift0.counts -m genmatrices/genmatrix-6-cpg.RData -o simseqs/sim-$DIR/test-cpg-fit-6-2-2.RData;
+        Rscript ../fit-model.R -c $MODEL -i simseqs/sim-$DIR/test-cpg-6-root-2-tip-l2-shift0.counts -m genmatrices/genmatrix-6-cpg.RData -o simseqs/sim-$DIR/test-cpg-fit-6-2-2.RData;
         Rscript ../gather-results.R --fit simseqs/sim-$DIR/test-cpg-fit-6-2-2.RData --sim simseqs/sim-${DIR}/test-cpg.RData --outfile simseqs/sim-$DIR/test-cpg-fit-6-2-2.json --json 2>/dev/null 
     ) &
 done
