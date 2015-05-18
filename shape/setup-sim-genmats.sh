f62c08cd03b919a26a602623115249c6e8596019
peter <petrel.harp@gmail.com>
2015-05-18 16:43:30 -0700

    .

--- shape/setup-sim-genmats.sh
+++ shape/setup-sim-genmats.sh
@@ -3,14 +3,19 @@
 set -eu
 set -o pipefail
 
+if [ $# -lt 1]
+then
+    echo "Usage: setup-sim-genmats.sh (prefix to .json config file"
+    exit 1
+fi
+
+MODEL=$1
+
 BASEDIR="minimal"
 GMDIR="genmatrices"
 mkdir -p $GMDIR
 
-for MODEL in shape-model-MGW shape-model-all-variables 
-do
-    echo "Simulating from ${MODEL} ."
-    SIMFILE="$BASEDIR/sim_${MODEL}.RData"
-    SIMGENMAT="$GMDIR/sim-${MODEL}-genmatrix.RData"  # this takes a WHILE, so let's save it for future use
-    Rscript ../sim-seq.R -c $MODEL -t .0001 -s 20 -o $SIMFILE -m $SIMGENMAT
-done
+echo "Simulating from ${MODEL} ."
+SIMFILE="$BASEDIR/sim_${MODEL}.RData"
+SIMGENMAT="$GMDIR/sim-${MODEL}-genmatrix.RData"  # this takes a WHILE, so let's save it for future use
+Rscript ../sim-seq.R -c $MODEL.json -t .0001 -s 20 -o $SIMFILE -m $SIMGENMAT
