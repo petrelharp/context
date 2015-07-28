d69ce40dae7930bd2b1e9bf1f007eff5dd7c26d1
peter <petrel.harp@gmail.com>
2015-07-27 21:29:12 -0700

    .

--- input-output.R
+++ input-output.R
@@ -141,9 +141,11 @@ treeify.config <- function (config,tlen=NULL) {
 
 check.genmatrix <- function (config,genmatrix) {
     # check that genmatrix is compatible with config
+    # TODO: check that fixfn agrees too
     for (xname in c("bases", "mutpats", "selpats", "selfactors")) {
         if (!isTRUE(all.equal( slot(genmatrix,xname), config$xname ))) {
             stop("Precomputed generator matrix does not agree with configuration values for:", xname)
+        }
     }
     return(TRUE)
 }
