ba3a46580b296cd950538e775756c994598c7122
peter <petrel.harp@gmail.com>
2015-08-04 09:38:42 -0700

    likelihood surface

---
+++ likelihood-surface.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+usage="Usage: $0 CONFIG OUTFILE longwin=INT shortwin=INT leftwin=INT tlen=NUMERIC [VAR=VALUE [VAR=VALUE]]"
+
+test "$#" -ge 2 || {
+  echo $usage >&2
+  exit 2
+}
+
+pandoc -v >/dev/null || {
+    echo "No working pandoc found.  Exiting."
+    exit 3
+}
+
+template="$(dirname $0)/likelihood-surface.Rmd"
+config="$(pwd)/$1"
+outfile="$(pwd)/$2"
+shift
+shift
+
+mkdir -p $(dirname $outfile)
+
+# read does NOT play well with set -eu 
+read -r -d '' SCRIPT << EOF
+library("rmarkdown")
+modelfile <- "${config}"
+$(for x in "$@"; do echo $x; done)
+render("$template", output_file="$outfile")
+EOF
+
+echo "$SCRIPT"
+
+echo "$SCRIPT" | R --vanilla --slave 
