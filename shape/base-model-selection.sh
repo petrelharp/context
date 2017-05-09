01f4d23f4c5f5a31b8ad566c8f98259cee51e4b4
peter <petrel.harp@gmail.com>
2017-05-08 21:54:08 -0700

    .

--- shape/base-model-selection.sh
+++ shape/base-model-selection.sh
@@ -5,15 +5,38 @@ TYPES="CTCF_binding_site  enhancer  open_chromatin_region  promoter_flanking_reg
 
 ## Base model:
 # get the correct base frequencies in there:
-for d in $DIRS; do for t in $TYPES; do 
-    CG=$(grep "$(basename $d)/$t" RegulatoryFeature-regions-from-axt/initfreqs.txt | cut -f 2 -d ' ')
-    # JSON needs leading 0.'s ...
-    A=0$(bc -l <<< "scale=4; (1-$CG)/2.0")
-    C=0$(bc -l <<< "scale=4; $CG/2.0")
-    G=0$(bc -l <<< "scale=4; $CG/2.0")
-    T=0$(bc -l <<< "scale=4; (1-$CG)/2.0")
-    cat base-model.json | sed -e 's_genmatrices_../../../genmatrices_' | sed -e "s/0.25, 0.25, 0.25, 0.25/$A, $C, $G, $T/" > $d/$t/base-model.json; 
-done; done
+
+modelsetup () {
+    for d in $DIRS; do for t in $TYPES; do 
+        CG=$(grep "$(basename $d)/$t" RegulatoryFeature-regions-from-axt/initfreqs.txt | cut -f 2 -d ' ')
+        # JSON needs leading 0.'s ...
+        A=0$(bc -l <<< "scale=4; (1-$CG)/2.0")
+        C=0$(bc -l <<< "scale=4; $CG/2.0")
+        G=0$(bc -l <<< "scale=4; $CG/2.0")
+        T=0$(bc -l <<< "scale=4; (1-$CG)/2.0")
+        cat models/$1 | sed -e 's_genmatrices_../../../genmatrices_' | sed -e "s/0.25, 0.25, 0.25, 0.25/$A, $C, $G, $T/" > $d/$t/$1; 
+    done; done
+}
+
+modelsetup base-model.json
+
+# for fitting models, later
+fitmodel () {
+    MODELFILE=$1
+    TMER=$2
+    for d in $DIRS; do for t in $TYPES; do 
+        FITFILE=$d/$t/$TMER/${MODELFILE%.json}-fit.RData
+        if ! [ -e $FITFILE ]
+        then
+            ../scripts/fit-tree-model.R -i $d/$t/$TMER/total.counts.gz -c $d/$t/$MODELFILE -o $FITFILE
+            ../scripts/gather-results.R --json -f $FITFILE > ${FITFILE%RData}json
+            ../scripts/compute-resids.R -i $FITFILE -o ${FITFILE%.RData}-resids.2.1.l1.tsv -w 2 -s 1 -l 1 --pretty 
+            ../scripts/compute-resids.R -i $FITFILE -o ${FITFILE%.RData}-resids.2.1.l0.tsv -w 2 -s 1 -l 0 --pretty 
+            ../scripts/compute-resids.R -i $FITFILE -o ${FITFILE%.RData}-resids.3.1.l1.tsv -w 3 -s 1 -l 1 --pretty 
+        fi
+        echo $FITFILE
+    done; done
+}
 
 # collapse down to 6-3-2
 collapse () {
@@ -55,19 +78,9 @@ LONGWIN=4
 ../scripts/make-genmat.R -c base-model.json -w $LONGWIN -o genmatrices/base-model-genmatrix-${LONGWIN}.RData
 
 # fit model
+
 TMER="4-2-1"
-for d in $DIRS; do for t in $TYPES; do 
-    FITFILE=$d/$t/$TMER/base-model-fit.RData
-    if ! [ -e $FITFILE ]
-    then
-        ../scripts/fit-tree-model.R -i $d/$t/$TMER/total.counts.gz -c $d/$t/base-model.json -o $FITFILE
-        ../scripts/gather-results.R --json -f $FITFILE > ${FITFILE%RData}json
-    else
-        ../scripts/compute-resids.R -i $FITFILE -o ${FITFILE%.RData}-resids.2.1.l1.tsv -w 2 -s 1 -l 1 --pretty 
-        ../scripts/compute-resids.R -i $FITFILE -o ${FITFILE%.RData}-resids.2.1.l0.tsv -w 2 -s 1 -l 0 --pretty 
-        ../scripts/compute-resids.R -i $FITFILE -o ${FITFILE%.RData}-resids.3.1.l1.tsv -w 3 -s 1 -l 1 --pretty 
-    fi
-done; done
+fitmodel base-model.json $TMER
 
 # clearly, need to add CpG
 (for d in $DIRS; do for t in $TYPES; do 
@@ -88,6 +101,7 @@ done; done)
 # look at all the results
 ../scripts/collect-params-results.R $(for d in $DIRS; do for t in $TYPES; do find $d/$t -name "*fit.json"; done; done)  > model-selection-results.tsv
 
+
 ######################################
 ## Revised model with CG->*
 ## including making base model strand-symmetric and setting rates to near inferred (on first pass)
@@ -95,15 +109,7 @@ done; done)
 # get the correct base frequencies in there:
 MODELFILE="base-model-plus-cpg.json"
 MODELNAME=${MODELFILE%.json}
-for d in $DIRS; do for t in $TYPES; do 
-    CG=$(grep "$(basename $d)/$t" RegulatoryFeature-regions-from-axt/initfreqs.txt | cut -f 2 -d ' ')
-    # JSON needs leading 0.'s ...
-    A=0$(bc -l <<< "scale=4; (1-$CG)/2.0")
-    C=0$(bc -l <<< "scale=4; $CG/2.0")
-    G=0$(bc -l <<< "scale=4; $CG/2.0")
-    T=0$(bc -l <<< "scale=4; (1-$CG)/2.0")
-    cat models/$MODELFILE | sed -e 's_genmatrices_../../../genmatrices_' | sed -e "s/0.25, 0.25, 0.25, 0.25/$A, $C, $G, $T/" > $d/$t/$MODELFILE; 
-done; done
+modelsetup $MODELFILE
 
 # create genmatrix
 LONGWIN=4
@@ -111,26 +117,43 @@ LONGWIN=4
 
 # fit model
 TMER="4-2-1"
-for d in $DIRS; do for t in $TYPES; do 
-    FITFILE=$d/$t/$TMER/${MODELNAME}-fit.RData
-    if ! [ -e $FITFILE ]
-    then
-        ../scripts/fit-tree-model.R -i $d/$t/$TMER/total.counts.gz -c $d/$t/$MODELFILE -o $FITFILE
-        ../scripts/gather-results.R --json -f $FITFILE > ${FITFILE%RData}json
-        ../scripts/compute-resids.R -i $FITFILE -o ${FITFILE%.RData}-resids.2.1.l1.tsv -w 2 -s 1 -l 1 --pretty 
-        ../scripts/compute-resids.R -i $FITFILE -o ${FITFILE%.RData}-resids.2.1.l0.tsv -w 2 -s 1 -l 0 --pretty 
-        ../scripts/compute-resids.R -i $FITFILE -o ${FITFILE%.RData}-resids.3.1.l1.tsv -w 3 -s 1 -l 1 --pretty 
-    fi
-done; done
+fitmodel $MODELFILE $TMER
 
-# clearly, need to add CpG
-(for d in $DIRS; do for t in $TYPES; do 
+(for d in $DIRS; do for t in $TYPES; do if [ -e $d/$t/$TMER/${MODELNAME}-fit-resids.2.1.l0.tsv ]; then
     echo $d $t; 
     head -n 5 $d/$t/$TMER/${MODELNAME}-fit-resids.2.1.l0.tsv | awk '{print $2"\t."$3"\t"$7}';
     tail -n 5 $d/$t/$TMER/${MODELNAME}-fit-resids.2.1.l0.tsv | awk '{print $2"\t."$3"\t"$7}';
     echo   "...."
     head -n 5 $d/$t/$TMER/${MODELNAME}-fit-resids.2.1.l1.tsv | awk '{print $2"\t"$3".\t"$7}';
     tail -n 5 $d/$t/$TMER/${MODELNAME}-fit-resids.2.1.l1.tsv | awk '{print $2"\t"$3".\t"$7}';
-done; done)
+fi; done; done)
+
+
+######################################
+## Worries that GC bias may introduce nonidentifiability
+## ...  as above but no GC bias
+
+# get the correct base frequencies in there:
+MODELFILE="base-model-plus-cpg-no-gc-bias.json"
+MODELNAME=${MODELFILE%.json}
+modelsetup $MODELFILE
+
+# create genmatrix
+LONGWIN=4
+../scripts/make-genmat.R -c models/$MODELFILE -w $LONGWIN -o genmatrices/${MODELNAME}-model-genmatrix-${LONGWIN}.RData
+
+# fit model
+TMER="4-2-1"
+FITFILES=$(fitmodel $MODELFILE $TMER)
+
+(for d in $DIRS; do for t in $TYPES; do if [ -e $d/$t/$TMER/${MODELNAME}-fit-resids.2.1.l0.tsv ]; then
+    echo $d $t; 
+    head -n 5 $d/$t/$TMER/${MODELNAME}-fit-resids.2.1.l0.tsv | awk '{print $2"\t."$3"\t"$7}';
+    tail -n 5 $d/$t/$TMER/${MODELNAME}-fit-resids.2.1.l0.tsv | awk '{print $2"\t."$3"\t"$7}';
+    echo   "...."
+    head -n 5 $d/$t/$TMER/${MODELNAME}-fit-resids.2.1.l1.tsv | awk '{print $2"\t"$3".\t"$7}';
+    tail -n 5 $d/$t/$TMER/${MODELNAME}-fit-resids.2.1.l1.tsv | awk '{print $2"\t"$3".\t"$7}';
+fi; done; done)
+
 
 
